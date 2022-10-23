dd4e83a69b328725b9525a54aa5ee21816ac8e1f
Javier Chavez <javcho@skrin.tech>
2022-10-23 14:48:58 -0500

    Update pages to have only login a home

--- src/components/LoginWelcome.vue
+++ src/components/LoginWelcome.vue
@@ -6,13 +6,8 @@ defineProps<{
 
 <template>
   <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
-      What's next?
-    </h3>
+    <h1 class="green">Bienvenido a Gym Tracker</h1>
+    <h3>Vamos a potenciar el seguimiento de tu rutina. ¿Estás listo?</h3>
   </div>
 </template>
 
