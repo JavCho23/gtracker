78ede9f1e29d7cbd5e2340a15bce9fa84cd13f7e
Javier Chavez <javcho@skrin.tech>
2022-10-24 00:27:35 -0500

    Updated first name in home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,10 +2,11 @@
 import { userState } from "@/state/user";
 
 const user = userState.user;
+const firstName = user?.displayName?.split(" ")[0];
 </script>
 
 <template>
-  <h1>Bienvenido {{ user?.displayName }}</h1>
+  <h1>Bienvenido(a) {{ firstName }}</h1>
   <br />
   <h3>Estamos construyendo el tracker...</h3>
 </template>
