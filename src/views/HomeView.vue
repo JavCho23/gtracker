cbc5ef29848606e862e4f25401c540102a739498
Javier Chavez <javcho@skrin.tech>
2022-10-27 22:44:40 -0500

    Button to add exercise added

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,22 @@
 <script setup lang="ts">
 import HomeHeader from "@/components/HomeHeader.vue";
 import TrainingList from "@/components/training/TrainingList.vue";
+import Button from "primevue/button";
+
+function onAddClick() {
+  alert("Agregar un entrenamiento");
+}
 </script>
 
 <template>
   <main class="p-4">
     <HomeHeader />
     <TrainingList />
+    <Button
+      icon="pi pi-plus"
+      class="p-button-raised p-button-rounded fixed bottom-0 right-0 m-5 shadow-6"
+      style="height: 3em; width: 3em"
+      @click="onAddClick"
+    />
   </main>
 </template>
